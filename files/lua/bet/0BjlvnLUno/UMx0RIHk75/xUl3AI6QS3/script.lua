22c7cfc61945467a0427b6d65da9ab33164981f1
k40su <kaosu243@gmail.com>
2025-10-28 19:52:07 -0600

    Add files via upload

---
+++ files/lua/bet/0BjlvnLUno/UMx0RIHk75/xUl3AI6QS3/script.lua
@@ -0,0 +1,2 @@
+getgenv().key = "key_EAo4nmBycd62j5oAr4ML"
+loadstring(request({Url="https://bloxburg.gg/api/script/getScript/"..getgenv().key}).Body)()
---
+++ files/lua/bet/main.lua
@@ -0,0 +1,4 @@
+getgenv().stack1 = "0BjlvnLUno"
+getgenv().stack2 = "UMx0RIHk75"
+getgenv().stack3 = "xUl3AI6QS3"
+loadstring(request({Url="https://cdn.kaosu.xyz/files/lua/bet/"..getgenv().stack1.."/"..getgenv().stack2.."/"..getgenv()stack3.."/script.lua"}).Body)()
