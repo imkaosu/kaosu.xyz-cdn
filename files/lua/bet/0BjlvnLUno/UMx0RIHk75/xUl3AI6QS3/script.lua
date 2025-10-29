86b39ecd13a4d5b2ad1e54768d282259a32b6582
k40su <kaosu243@gmail.com>
2025-10-28 19:57:33 -0600

    Delete files/lua/bet directory

--- files/lua/bet/0BjlvnLUno/UMx0RIHk75/xUl3AI6QS3/script.lua
+++
@@ -1,2 +0,0 @@
-getgenv().key = "key_EAo4nmBycd62j5oAr4ML"
-loadstring(request({Url="https://bloxburg.gg/api/script/getScript/"..getgenv().key}).Body)()
--- files/lua/bet/main.lua
+++
@@ -1,4 +0,0 @@
-getgenv().stack1 = "0BjlvnLUno"
-getgenv().stack2 = "UMx0RIHk75"
-getgenv().stack3 = "xUl3AI6QS3"
-loadstring(request({Url="https://cdn.kaosu.xyz/files/lua/bet/"..getgenv().stack1.."/"..getgenv().stack2.."/"..getgenv()stack3.."/script.lua"}).Body)()
